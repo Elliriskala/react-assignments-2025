f1f68f43637925e495e136bde3af8be461ce848f
ellinor <ellinoora.riskala@metropolia.fi>
2025-01-23 11:18:06 +0200

    feat: add testing setup with Vitest and implement tests for Profile and Upload components

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/~ellinor/hybrid-react-build/'
+  base: '/~ellinor/hybrid-react-build/',
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './vitest-setup.js',
+  },
 })
