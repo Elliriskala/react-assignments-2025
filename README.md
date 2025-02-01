a09802307d0d10131659eca0e51c72fe9ece9529
ellinor <ellinoora.riskala@metropolia.fi>
2025-02-01 18:46:09 +0200

    feat: update README and Vite config for new deployment path

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 ## My media app
 
 ### Here you can view my media app,
-### Open: https://users.metropolia.fi/~ellinor/hooks/
+### Open: https://users.metropolia.fi/~ellinor/forms/
--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/~ellinor/hooks/',
+  base: '/~ellinor/forms/',
   test: {
     environment: 'jsdom',
     globals: true,
